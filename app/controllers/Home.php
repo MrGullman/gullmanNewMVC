bea504ba3f90fe10eb7b0d2740aaf42ceb11b5a5
Jesper Gullman <jesper.gullman@outlook.com>
2019-05-24 21:23:07 +0200

    DB wrapper ok

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -6,11 +6,33 @@ class Home extends Controller
     public function __construct($controller, $action)
     {
         parent::__construct($controller, $action);
-        
+
     }
 
     public function indexAction()
     {
+        $db = DB::getInstance();
+        $fields = [
+            'fname' => 'Kajsa',
+            'email' => 'kajsaAnka@ankeborg.com'
+        ];
+        // $sql = "SELECT * FROM contacts";
+        $contacts = $db->find('contacts', [
+            'conditions' => "lname = ?",     // Use String
+            // 'conditions' => ['fname = ?','lname = ?'],  // Use Array
+            // 'bind' => ['Jesper', 'Gullman'],             // Use with Array
+            'bind' => ['Gullman'],
+            'order' => "lname",
+            'limit' => 5
+        ]);
+
+        $contactsFirst = $db->findFirst('contacts', [
+            'conditions' => "lname = ?",     // Use String
+            'bind' => ['Gullman']
+        ]);
+        // $contact = $db->query("SELECT * FROM contacts ORDER BY lname, fname")->lastID();
+
+        dnd($contactsFirst);
         $this->view->render('home/index');
     }
 }
--- config/config.php
+++ config/config.php
@@ -1,9 +1,16 @@
 <?php
 
-// MVC SETTINGS
-
+// Debug
 define('DEBUG', true);
 
+// Database Settings
+define('DB_HOST', '127.0.0.1'); // Database host ***Use IP address to avoid DNS lookup
+define('DB_NAME', 'gullmanmvc'); // Database name
+define('DB_USER', 'root');  // Database user
+define('DB_PASS', '1234');  // Database password
+define('DB_CHAR', 'utf8mb4'); // Database charset
+
+// MVC Settings
 define('DEFAULT_CONTROLLER', 'Home');   // Default controller if none is set.
 define('DEFAULT_LAYOUT', 'default');     // If no layout is set in the controller use this layout.
 
