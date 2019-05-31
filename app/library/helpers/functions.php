e416ea633c54d9f337e0030ffa9fdf2951af8fa0
Jesper Gullman <jesper.gullman@outlook.com>
2019-05-31 20:19:42 +0200

    Added formhelper $controller = ACCESS_RESTRICTED . 'Controller';

--- app/library/helpers/functions.php
+++ app/library/helpers/functions.php
@@ -1,4 +1,5 @@
 <?php
 require_once(ROOT . DS . 'app' .DS . 'library' . DS . 'helpers' . DS . 'helpers.php');
+require_once(ROOT . DS . 'app' .DS . 'library' . DS . 'helpers' . DS . 'form_helpers.php');
 
 ?>
